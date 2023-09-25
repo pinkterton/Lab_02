1cd712d3d76c868f27185981b8f4d7f4586d5047
unknown <wotmstislav@gmail.com>
2023-09-25 21:52:22 +0300

    created new program

---
+++ Lab_02.cpp
@@ -0,0 +1,28 @@
+﻿// Lab_02.cpp
+// Кругліцький Мстислав
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 14
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double z1;
+	double z2;
+
+	cout << "Angle Alpha: "; cin >> x;
+
+	z1 = (cos(x) + sin(x)) / (cos(x) - sin(x));
+	z2 = (tan(x) * tan(x)) + ((1 / cos(x)) * (1 / cos(x))); // sec = 1/cos(x)
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cout << "Congratulation!" << endl;
+
+	return 0;
+}
